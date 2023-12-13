70e6d4bf7196779b2ef3f7fb90af2662208e028a
niclas <nicjan01@gmail.com>
2023-12-13 15:56:08 +0100

    Add flags

--- deploy/update_misp.sh
+++ deploy/update_misp.sh
@@ -8,6 +8,29 @@ RED='\033[0;31m'
 VIOLET='\033[0;35m'
 NC='\033[0m' # No Color
 
+setDefaultArgs(){
+    default_misp_img="../build/images/misp.tar.gz"
+    default_current_misp=""
+    default_new_misp=$(generateName "misp")
+
+    default_php_ini="yes"
+
+    default_mysql="no"
+    default_mysql_img="../build/images/mysql.tar.gz"
+    #default_current_mysql=""
+    default_new_mysql=$(generateName "mysql")
+
+    default_redis="no"
+    default_redis_img="../build/images/redis.tar.gz"
+    #default_current_redis=""
+    default_new_redis=$(generateName "redis")
+    
+    default_modules="no"
+    default_modules_img="../build/images/modules.tar.gz"
+    #default_current_modules=""
+    default_new_modules=$(generateName "modules")
+}
+
 error() {
     local msg=$1
     echo -e "${RED}Error: $msg${NC}" > /dev/tty
@@ -23,21 +46,12 @@ okay() {
     echo -e "${GREEN}Info: $msg${NC}" > /dev/tty
 }
 
-usage() {
-  echo "Usage: $0 -n <old-container-name> -f <image> [-o <new-container-name>]"
-  echo "Options:"
-  echo "  -n <old-container-name>  Set the name of the container to be updated"
-  echo "  -f <path-to-image-file>  Set path to the new image file"
-  echo "  -o <new-container-name>  Set the name of the updated container"
-  exit 1
-}
-
 generate_name(){
     local name="$1"
     echo "${name}-$(date +%Y%m%d%H%M%S)"
 }
 
-check_resource_exists() {
+checkResourceExists() {
     local resource_type="$1"
     local resource_name="$2"
 
@@ -87,60 +101,189 @@ checkSoftwareDependencies() {
     done
 }
 
-checkSoftwareDependencies
+usage(){
+    echo "TODO"
+}
 
-while getopts ":hn:f:p:o:" opt; do
-  case $opt in
-    n)
-      MISP_CONTAINER="$OPTARG"
-      ;;
-    f)
-      FILE="$OPTARG"
-      ;;
-    o)
-      NEW_CONTAINER="$OPTARG"
-      ;;
-    h)
-      usage
-      ;;
-    \?)
-      echo "Invalid option: -$OPTARG" >&2
-      usage
-      ;;
-    :)
-      echo "Option -$OPTARG requires an argument." >&2
-      usage
-      ;;
-  esac
+nonInteraciveConfig(){
+    ALL=false
+    VALID_ARGS=$(getopt -o ic:n:a --long interactive,no-php-ini,misp-image:,current-misp:,new-misp:,all,update-mysql,mysql-image:,new-mysql:,update-redis,redis-image:,new-redis:,update-modules,modules-image:,new-modules:  -- "$@")
+    if [[ $? -ne 0 ]]; then
+        exit 1;
+    fi
+
+    eval set -- "$VALID_ARGS"
+    while [ $# -gt 0 ]; do
+        case "$1" in
+            -i | --interactive)
+                INTERACTIVE=true
+                break
+                ;;
+            --no-php-ini)
+                php_ini="n"
+                shift
+                ;;
+            --misp-image)
+                misp_img=$2
+                shift 2
+                ;;
+            -c | --current-misp)
+                current_misp=$2
+                shift 2
+                ;;
+            -n | --new-misp)
+                new_misp=$2
+                shift 2
+                ;;
+            -a | --all)
+                ALL=true
+                shift
+                ;;
+            --update-mysql)
+                mysql="y"
+                shift
+                ;;
+            --mysql-image)
+                mysql_img=$2
+                shift 2
+                ;;
+            --new-mysql)
+                new_mysql=$2
+                shift 2
+                ;;
+            --update-redis)
+                redis="y"
+                shift
+                ;;
+            --redis-image)
+                redis_img=$2
+                shift 2
+                ;;
+            --new-redis)
+                new_redis=$2
+                shift 2
+                ;;
+            --update-modules)
+                modules=$2
+                shift
+                ;;
+            --modules-image)
+                modules_img=$2
+                shift 2
+                ;;
+            --new-modules)
+                new_modules=$2
+                shift 2
+                ;;
+            *)  
+                break 
+                ;;
+        esac
+    done
+
+    # Set global values
+    MISP_IMAGE=${misp_img:-$default_misp_img}
+    CURRENT_MISP=${current_misp:-$default_current_misp}
+    NEW_MISP=${new_misp:-$default_new_misp}
+
+    MYSQL=${mysql:-$default_mysql}
+    MYSQL_IMAGE=${mysql_img:-$default_mysql_img}
+    NEW_MYSQL=${new_mysql:-$default_new_mysql}
+
+    REDIS=${redis:-$default_redis}
+    REDIS_IMAGE=${redis_img:-$default_redis_img}
+    NEW_REDIS=${new_redis:-$default_new_redis}
+
+    MODULES=${modules:-$default_modules}
+    MODULES_IMAGE=${modules_img:-$default_modules_img}
+    NEW_MODULES=${new_modules:-$default_new_modules}
+
+    php_ini=${php_ini:-$default_php_ini}
+    PHP_INI=$(echo "$php_ini" | grep -iE '^y(es)?$' > /dev/null && echo true || echo false)
+}
+
+checkNamingConvention(){
+    local input="$1"
+    local pattern="^[a-zA-Z0-9-]+$"
+
+    if ! [[ "$input" =~ $pattern ]]; then
+        return 1
+    fi
+    return 0
+}
+
+validateArgs(){
+    # Names
+    if ! checkNamingConvention $NEW_MISP; then
+        error "Name $NEW_MISP for MISP container is not valid. Please use only alphanumeric characters and hyphens."
+    fi
+    if MYSQL && ! checkNamingConvention $NEW_MYSQL; then
+        error "Name $NEW_MYSQL for MySQL container is not valid. Please use only alphanumeric characters and hyphens."
+    fi
+    if REDIS && ! checkNamingConvention $NEW_REDIS; then
+        error "Name $NEW_REDIS for Redis container is not valid. Please use only alphanumeric characters and hyphens."
+    fi
+    if MODULES && ! checkNamingConvention $NEW_MODULES; then
+        error "Name $NEW_MODULES for Modules container is not valid. Please use only alphanumeric characters and hyphens."
+    fi
+
+    # Check current container
+    if ! checkResourceExists "container" $CURRENT_MISP; then
+        error "Container $CURRENT_MISP could not be found!"
+    fi
+
+    # Files
+    
+
+}
+
+# main
+checkSoftwareDependencies
+setDefaultArgs
+
+# Check for interactive install
+INTERACTIVE=false
+for arg in "$@"; do
+    if [[ $arg == "-i" ]] || [[ $arg == "--interactive" ]]; then
+        INTERACTIVE=true
+        break
+    fi
 done
 
-if [ -z "$MISP_CONTAINER" ] || [ -z "$FILE" ]; then
-  error "Both -n and -f options are mandatory." >&2
-  usage
+if [ "$INTERACTIVE" = true ]; then
+    interactiveConfig
+else
+    nonInteractiveConfig "$@"
 fi
 
-if ! check_resource_exists "container" $MISP_CONTAINER; then
-  error "Container does not exist. Please select the container running MISP."
-  exit 1
-fi
+validateArgs
+# if [ -z "$CURRENT_MISP" ] || [ -z "$FILE" ]; then
+#   error "Both -n and -f options are mandatory." >&2
+#   usage
+# fi
 
-if check_resource_exists "container" $NEW_CONTAINER; then
-    error "New container with name $NEW_CONTAINER already exists. Please use a new name."
-    exit 1
-fi
+# if ! checkResourceExists "container" $CURRENT_MISP; then
+#   error "Container does not exist. Please select the container running MISP."
+#   exit 1
+# fi
 
-if [ ! -f "$FILE" ]; then
-    error "The specified image file does not exist."
-    exit 1
-fi
+# if checkResourceExists "container" $NEW_MISP; then
+#     error "New container with name $NEW_MISP already exists. Please use a new name."
+#     exit 1
+# fi
+
+# if [ ! -f "$FILE" ]; then
+#     error "The specified image file does not exist."
+#     exit 1
+# fi
 
 PROJECT_NAME=$(get_current_lxd_project)
 
-if [ -z "$NEW_CONTAINER" ]; then
-    NEW_CONTAINER=$(generate_name "misp")
-    NEW_IMAGE=$(generate_name "misp")
+if [ -z "$NEW_MISP" ]; then
+    NEW_MISP=$(generate_name "misp")
+    MISP_IMAGE=$(generate_name "misp")
 else
-    NEW_IMAGE=$(generate_name "$NEW_CONTAINER")
+    MISP_IMAGE=$(generate_name "$NEW_MISP")
 fi
 
 # check if image fingerprint already exists
@@ -154,7 +297,7 @@ done
 
 # check if image alias already exists
 for image in $(lxc image list --project="${PROJECT_NAME}" --format=json | jq -r '.[].aliases[].name'); do
-    if [ "$image" = "$NEW_IMAGE" ]; then
+    if [ "$image" = "$MISP_IMAGE" ]; then
         error "Image Name already exists."
         exit 1
     fi
@@ -162,7 +305,7 @@ done
 
 # check if cotainer name already exists
 for container in $(lxc query "/1.0/images?recursion=1&project=${PROJECT_NAME}" | jq .[].alias -r); do
-    if [ "$container" = "$NEW_CONTAINER" ]; then
+    if [ "$container" = "$NEW_MISP" ]; then
         error "Container Name already exists."
         exit 1
     fi
@@ -184,50 +327,54 @@ lxc project switch $PROJECT_NAME
 
 # Pull config
 echo "Extract config..."
-lxc file pull -r $MISP_CONTAINER/var/www/MISP/app/files /tmp/$temp -v
-lxc file pull -r $MISP_CONTAINER/var/www/MISP/app/tmp /tmp/$temp -v
-lxc file pull -r $MISP_CONTAINER/var/www/MISP/app/Config /tmp/$temp -v
-lxc file pull -r $MISP_CONTAINER/var/www/MISP/app/webroot/img /tmp/$temp/webroot/ -v
-lxc file pull $MISP_CONTAINER/var/www/MISP/app/webroot/gpg.asc /tmp/$temp/webroot/ -v
-lxc file pull -r $MISP_CONTAINER/var/www/MISP/app/View/Emails/html/Custom /tmp/$temp/View/Emails/html/ -v
-lxc file pull -r $MISP_CONTAINER/var/www/MISP/app/View/Emails/text/Custom /tmp/$temp/View/Emails/text/ -v
-lxc file pull $MISP_CONTAINER/var/www/MISP/app/Plugin/CakeResque/Config/config.php /tmp/$temp/Plugin/CakeResque/Config/ -v
+lxc file pull -r $CURRENT_MISP/var/www/MISP/app/files /tmp/$temp -v
+lxc file pull -r $CURRENT_MISP/var/www/MISP/app/tmp /tmp/$temp -v
+lxc file pull -r $CURRENT_MISP/var/www/MISP/app/Config /tmp/$temp -v
+lxc file pull -r $CURRENT_MISP/var/www/MISP/app/webroot/img /tmp/$temp/webroot/ -v
+lxc file pull $CURRENT_MISP/var/www/MISP/app/webroot/gpg.asc /tmp/$temp/webroot/ -v
+lxc file pull -r $CURRENT_MISP/var/www/MISP/app/View/Emails/html/Custom /tmp/$temp/View/Emails/html/ -v
+lxc file pull -r $CURRENT_MISP/var/www/MISP/app/View/Emails/text/Custom /tmp/$temp/View/Emails/text/ -v
+lxc file pull $CURRENT_MISP/var/www/MISP/app/Plugin/CakeResque/Config/config.php /tmp/$temp/Plugin/CakeResque/Config/ -v
 okay "pulled files"
 
 # stop current MISP container
-lxc stop $MISP_CONTAINER
+lxc stop $CURRENT_MISP
 okay "container stopped"
 
 
 # Import new image
-lxc image import $FILE --alias $NEW_IMAGE
+lxc image import $FILE --alias $MISP_IMAGE
 okay "Image imported"
 
 # Create new instance
-profile=$(lxc config show $MISP_CONTAINER | yq eval '.profiles | join(" ")' -)
-lxc launch $NEW_IMAGE $NEW_CONTAINER --profile=$profile
+profile=$(lxc config show $CURRENT_MISP | yq eval '.profiles | join(" ")' -)
+lxc launch $MISP_IMAGE $NEW_MISP --profile=$profile
 okay "New conatiner created"
 
 
 # Transfer files to new instance
 echo "push config to new instance ..."
-lxc file push -r /tmp/$temp/files $NEW_CONTAINER/var/www/MISP/app/ -v
-lxc file push -r /tmp/$temp/tmp $NEW_CONTAINER/var/www/MISP/app/ -v
-lxc file push -r /tmp/$temp/Config $NEW_CONTAINER/var/www/MISP/app/ -v
-lxc file push -r /tmp/$temp/webroot/img $NEW_CONTAINER/var/www/MISP/app/webroot/ -v
-lxc file push /tmp/$temp/webroot/gpg.asc $NEW_CONTAINER/var/www/MISP/app/webroot/ -v
-lxc file push -r /tmp/$temp/View/Emails/html/Custom $NEW_CONTAINER/var/www/MISP/app/View/Emails/html/ -v
-lxc file push -r /tmp/$temp/View/Emails/text/Custom $NEW_CONTAINER/var/www/MISP/app/View/Emails/text/ -v
-lxc file push /tmp/$temp/Plugin/CakeResque/Config/config.php $NEW_CONTAINER/var/www/MISP/app/Plugin/CakeResque/Config/ -v
+lxc file push -r /tmp/$temp/files $NEW_MISP/var/www/MISP/app/ -v
+lxc file push -r /tmp/$temp/tmp $NEW_MISP/var/www/MISP/app/ -v
+lxc file push -r /tmp/$temp/Config $NEW_MISP/var/www/MISP/app/ -v
+lxc file push -r /tmp/$temp/webroot/img $NEW_MISP/var/www/MISP/app/webroot/ -v
+lxc file push /tmp/$temp/webroot/gpg.asc $NEW_MISP/var/www/MISP/app/webroot/ -v
+lxc file push -r /tmp/$temp/View/Emails/html/Custom $NEW_MISP/var/www/MISP/app/View/Emails/html/ -v
+lxc file push -r /tmp/$temp/View/Emails/text/Custom $NEW_MISP/var/www/MISP/app/View/Emails/text/ -v
+lxc file push /tmp/$temp/Plugin/CakeResque/Config/config.php $NEW_MISP/var/www/MISP/app/Plugin/CakeResque/Config/ -v
 okay "pushed files"
 
 # Set permissions
-lxc exec $NEW_CONTAINER -- sudo chown -R www-data:www-data /var/www/MISP/
-lxc exec $NEW_CONTAINER -- sudo chmod -R 775 /var/www/MISP/
+lxc exec $NEW_MISP -- sudo chown -R www-data:www-data /var/www/MISP/
+lxc exec $NEW_MISP -- sudo chmod -R 775 /var/www/MISP/
 
-# Update DB
-lxc exec $NEW_CONTAINER -- bash -c 'sudo -u "www-data" -H sh -c "/var/www/MISP/app/Console/cake Admin runUpdates"'
+# Update
+lxc exec $NEW_MISP -- bash -c 'sudo -u "www-data" -H sh -c "/var/www/MISP/app/Console/cake Admin runUpdates"'
 
 # Cleanup: Remove the temporary directory
 rm -r "$temp"
 okay "Removed temporary directory."
+
+# Add mysql config change 
+# update order?
+#
